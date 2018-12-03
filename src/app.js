0f5340186ad427116f8d0293cc58b0dac4d3aa78
George Salu <george.salu10@gmail.com>
2018-12-03 19:25:47 -0200

    deve separar camada de servico

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.use(knexLogger(app.db));
 
 consign({ cwd: 'src', verbose: false })
     .include('./config/middleware.js')
+    .then('./services')
     .then('./routes')
     .then('./config/routes.js')
     .into(app);
