65b5d8749ccb31d96a989bcb4a62a9c107486ec4
George Salu <george.salu10@gmail.com>
2018-12-05 20:32:59 -0200

    deve inserir um conta com sucesso

--- test/routes/account.test.js
+++ test/routes/account.test.js
@@ -4,8 +4,8 @@ const app = require('../../src/app');
 const MAIN_ROUTE = '/accounts';
 let user;
 
-beforeAll(() => {
-    const res = app.services.user.save({ name: 'User Account', mail: `${Date.now()}@gmail.com`, passwd: '123456' })
+beforeAll(async () => {
+    const res = await app.services.user.save({ name: 'User Account', mail: `${Date.now()}@gmail.com`, passwd: '123456' })
     user = { ...res[0] };
 })
 
