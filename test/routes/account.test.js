c854eebae44b75c7ae045c859947a550c8ffe8d8
George Salu <george.salu10@gmail.com>
2018-12-06 20:45:26 -0200

    deve alterar uma conta

--- test/routes/account.test.js
+++ test/routes/account.test.js
@@ -38,3 +38,14 @@ test('deve retornar uma conta por id', () => {
             expect(res.body.user_id).toBe(user.id)
         })
 })
+
+test('deve alterar uma conta', () => {
+    return app.db('accounts')
+        .insert({ name: 'Acc To Update', user_id: user.id }, ['id'])
+        .then(acc => request(app).get(`${MAIN_ROUTE}/${acc[0].id}`)
+            .send({ name: 'Acc Update' }))
+        .then((res) => {
+            expect(res.status).toBe(200);
+            expect(res.body.name).toBe('Acc To Update')
+        })
+})
