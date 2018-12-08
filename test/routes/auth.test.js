c124c9485c846263f160c6232024aeec323f1168
George Salu <george.salu10@gmail.com>
2018-12-08 15:10:29 -0200

    deve receber token ao logar

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -2,8 +2,8 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 test('deve receber token ao logar', () => {
-    return app.services.user.save(
-        { name: 'Walter Mitty', mail: `${Date.now()}@mail.com`, passwd: '123456' })
+    return app.services.user
+        .save({ name: 'Walter Mitty', mail: `${Date.now()}@mail.com`, passwd: '123456' })
         .then(() => request(app).post('/auth/signin')
             .send({ mail, passwd: '123456' }))
         .then((res) => {
