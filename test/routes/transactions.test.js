c385b463cee1520386ce093b54bca3ef0298a87d
George Salu <george.salu10@gmail.com>
2019-01-14 19:35:52 -0200

    correção teste de criação de transacao

--- test/routes/transactions.test.js
+++ test/routes/transactions.test.js
@@ -51,7 +51,7 @@ test('deve funcionar com snippets', () => {
 test('deve inserir uma transação com sucesso', () => {
     return request(app).post(MAIN_ROUTE)
         .set('authorization', `bearer ${user.token}`)
-        .send({ description: 'New T', date: Date(), ammount: 100, type: 'I', acc_id: accUser.id })
+        .send({ description: 'T1', date: new Date(), ammount: 100, type: 'I', acc_id: accUser.id })
         .then((res) => {
             expect(res.status).toBe(201)
             expect(res.body.acc_id).toBe(accUser.id)
