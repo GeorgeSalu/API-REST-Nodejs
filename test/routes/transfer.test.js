b83889afbd09dbeeabc2db4d4c7664b33e737ab0
George Salu <george.salu10@gmail.com>
2019-02-26 23:46:04 -0300

    deve inserir uma transaferencia com sucesso

--- test/routes/transfer.test.js
+++ test/routes/transfer.test.js
@@ -22,3 +22,21 @@ test('deve listar apenas as transferencias do usuario', () => {
         })
 })
 
+test('deve inserir uma transferencia com sucesso', () => {
+    return request(app).post(MAIN_ROUTE)
+        .set('authorization', `bearer ${TOKEN}`)
+        .send({ description: 'regular transfer', user_id: 10000, acc_ori_id: 10000, acc_dest_id: 10001, ammount: 100, date: new Date() })
+        .then(async (res) => {
+            expect(res.status).toBe(201);
+            expect(res.body.description).toBe('regular transfer');
+
+            const transactions = await app.db('transactions').where({ transfer_id: res.body.id });
+
+            expect(transactions).toHaveLength(2);
+            expect(transactions[0].description).toBe('transfer to acc 10001');
+            expect(transactions[0].ammount).toBe('-100.00');
+            expect(transactions[1].ammount).toBe('-100.00');
+            expect(transactions[0].acc_id).toBe(10000);
+            expect(transactions[1].acc_id).toBe(10000);
+        })
+})
